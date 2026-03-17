0467a1c631b3731b40201b3f8ab21028a228cd84
Peter Veentjer <peter.veentjer@weareadaptive.com>
2026-03-17 14:06:02 +0200

    Improved classProperty class not found. (#106)
    If a class isn't found, then you get an exception like this:
    
    java.lang.IllegalArgumentException: invalid class value for property 'io.aeron.benchmarks.message.transceiver', cause: io.aeron.benchmarks.aeron.EchoFanOutMessageTransceiver
           at io.aeron.benchmarks.Configuration.classProperty(Configuration.java:980)
           at io.aeron.benchmarks.Configuration.fromSystemProperties(Configuration.java:723)
           at io.aeron.benchmarks.LoadTestRig.main(LoadTestRig.java:391)
    
    It isn't immediately clear that the class could not be found since that information is missing from the error.
    
    This PR changes that by including the source exception.

--- benchmarks-api/src/main/java/io/aeron/benchmarks/Configuration.java
+++ benchmarks-api/src/main/java/io/aeron/benchmarks/Configuration.java
@@ -989,7 +989,7 @@ public final class Configuration
         catch (final ClassNotFoundException | ClassCastException ex)
         {
             throw new IllegalArgumentException(
-                "invalid class value for property '" + propName + "', cause: " + ex.getMessage());
+                "invalid class value for property '" + propName + "', cause: " + ex.getMessage(), ex);
         }
     }
 
